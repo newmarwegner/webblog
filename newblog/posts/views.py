38275b9c11a85f1bb86518e42d3f5fd5ebe83bd9
newmarwegner <newmarwegner@gmail.com>
2021-02-25 17:07:58 -0300

    links

--- newblog/posts/views.py
+++ newblog/posts/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q, Count, Case, When
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
@@ -9,14 +10,28 @@ class PostIndex(ListView):
     template_name = 'index.html'
     paginate_by = 3
     context_object_name = 'posts'
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id').filter(publicado_post=True)
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
+        
+        return qs
+
 
 class PostBusca(PostIndex):
     pass
 
+
 class PostCategoria(PostIndex):
     pass
 
+
 class PostDetalhes(UpdateView):
     pass
-
-
