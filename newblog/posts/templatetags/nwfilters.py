44eda9cf0e33a289a8f90467679d85b7ab25fdd9
newmarwegner <newmarwegner@gmail.com>
2021-02-26 17:07:13 -0300

    templatetags

---
+++ newblog/posts/templatetags/nwfilters.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='plural_comentarios')
+def plural_comentarios(num_comentarios):
+    if num_comentarios > 1:
+        return f'{num_comentarios} comentários'
+    elif num_comentarios == 1:
+        return f'{num_comentarios} comentário'
+    else:
+        return f'Sem comentários'
+
